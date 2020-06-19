80be2941b3ff9b5dcfcfe7de37348d40220bf757
blvnder <alicelinlc@gmail.com>
2020-06-19 15:45:05 -0700

    Connect to database

--- index.js
+++ index.js
@@ -2,9 +2,32 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const { Pool } = require('pg');
+const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+    connectionString: 'postgres://postgres:root@localhost/people',
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .use(express.static(path.join(__dirname, 'public')))
+    
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    
+    .get('/', async (req, res) => {
+        try {
+            const client = await pool.connect();
+            const result = await client.query('SELECT * FROM person');
+            const results = { 'results': (result) ? result.rows : null};
+            res.render('pages/index', results );
+            client.release();
+        } catch (err) {
+            console.error(err);
+            res.send("Error " + err);
+        }
+    })
+    
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
